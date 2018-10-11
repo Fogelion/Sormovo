4e472f41705dd831d7212f8700b4264510a37894
Fogelion <finoleggen@gmail.com>
2018-10-11 13:45:41 +0300

    s5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,10 @@ export default function Header() {
 
 	return (
 		<header className="Header">
-			<div className="headerLogo">
-				<a href="../../../public/index.html"><img src={require("../../image/adeptus_mechanicus.png")} alt="logo" id="headerLogo"/></a></div>
-			<div className="HeaderTit"><h1 className="HeaderName">Адептус Сормус</h1></div>
+			{/*<div className="headerLogo">*/}
+				{/*<a href="../../../public/index.html"><img src={require("../../image/adeptus_mechanicus.png")} alt="logo" id="headerLogo"/></a></div>*/}
+			{/*<div className="HeaderTit"><h1 className="HeaderName">Адептус Сормус</h1></div>*/}
+				<img src={require("../../image/URA_URA12.jpg")} alt="1"  className="imgTest"/>
 		</header>
 	);
 }
