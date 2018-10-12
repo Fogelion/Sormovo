23cf7ac4ecc86a7ed3d011edb91788ab04df8ffc
Fogelion <finoleggen@gmail.com>
2018-10-12 09:53:41 +0300

    шапка+задний фон+герб+заголовок(цвет и выделение)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,12 @@ export default function Header() {
 
 	return (
 		<header className="Header">
-			{/*<div className="headerLogo">*/}
-				{/*<a href="../../../public/index.html"><img src={require("../../image/adeptus_mechanicus.png")} alt="logo" id="headerLogo"/></a></div>*/}
-			{/*<div className="HeaderTit"><h1 className="HeaderName">Адептус Сормус</h1></div>*/}
-				<img src={require("../../image/URA_URA12.jpg")} alt="2"  className="imgTest"/>
+			<div className="headerLogo">
+				<a href="../../../public/index.html">
+					<img src={require("../../image/headerCrest.png")} alt="logo" id="headerLogo"/>
+				</a></div>
+			<div className="HeaderTit"><h1 className="HeaderName">Адептус Сормус</h1></div>
+				{/*<img src={require("../../image/headerBack.jpg")} alt="2"  className="imgTest"/>*/}
 		</header>
 	);
 }
