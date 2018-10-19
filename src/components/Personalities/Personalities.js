d42e4c1c399faa412ac110d6fae3e3699837e083
Fogelion <finoleggen@gmail.com>
2018-10-19 21:33:18 +0300

    examples v1

--- src/components/Personalities/Personalities.js
+++ src/components/Personalities/Personalities.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PersExamp from './PersExamp.js';
 
 
 export default function Personalities() {
+	// console.log(PersExamp[0].date.getFullYear() + '-' + PersExamp[0].date.getMonth() + '-' + PersExamp[0].date.getDate());
+	let formatDate = PersExamp[0].date.getFullYear() + '-' + PersExamp[0].date.getMonth() + '-' + PersExamp[0].date.getDate();
+
+	let testVAr1 = PersExamp[0].testMe;
 
 	return (
 		<div>
 			<h1>Personalities</h1>
+			<section className='postPersons'>
+				<header className='headerPersons'>
+					<h2>{PersExamp[0].title}</h2>
+					<time dateTime={formatDate}>{formatDate}</time>
+				</header>
+				<article className='contentPersons'>
+					<p dangerouslySetInnerHTML={{__html: PersExamp[0].text}}></p>
+				</article>
+			</section>
 		</div>
 	);
 }
